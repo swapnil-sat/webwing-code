27a2a4761866c22ffe4c5064ff5b5dc53d116bf6
manthanbore <mantha.bore@gmail.com>
2021-04-03 13:39:38 +0530

    HELLO JOHN

---
+++ ManthanBore/frozenset.py
@@ -0,0 +1,6 @@
+# s={10,20,30,40,50,60,70}
+# fs=frozenset(s)
+# print(fs)
+# print(type(fs))
+# print(id(fs))
+# print(len(fs))
---
+++ ManthanBore/tuple.py
@@ -0,0 +1,32 @@
+# tuple=(10,20,30,40,50)
+# print(tuple)
+# print(type(tuple))
+# print(id(tuple))
+# print(len(tuple))
+
+# b=tuple('''MAN''')
+# print(b)
+# print(type(b))
+# print(id(b))
+# print(len(b))
+
+# z=tuple("man")
+# print(z)
+# list=[10,20,'abc',True]
+# x=tuple(list)
+# print(x)
+# print(type(x))
+# print(id(x))
+# print(len(x))
+
+
+# c=(10,30,50,70,'ENG',"maths",'''Science''')
+# d=[25,50,75,'HISTORY',"geo",'''Economics''']
+# e=(tuple(d))
+# print(e)
+# print(type(e))
+# print(id(e))
+# print(len(e))
+
+# x = tuple()
+# print(type(x))
