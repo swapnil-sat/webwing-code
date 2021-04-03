4293c8f8b7aad8ecef6bff3aeffc301082d8cc83
manthanbore <mantha.bore@gmail.com>
2021-04-03 23:59:40 +0530

    ROGER

---
+++ ManthanBore/arithmetic operators.py
@@ -0,0 +1,59 @@
+# a=10
+# b=20
+# print(a+b)
+
+# a="WEB"
+# b="WING"
+# print(a+b)
+
+# c=50
+# d=30
+# print(c-d)
+
+# c=30
+# d=50
+# print(c-d)
+
+# i=7
+# j=7
+# print(i*j)
+
+# k=3
+# l="ABC"
+# print(k*l)
+
+# e=10
+# f=5
+# print(e/f)
+
+# e=7
+# f=3
+# print(e/f)
+
+# g=10
+# h=2
+# print(g//h)
+
+# g=10
+# h=50
+# print(g//h)
+
+# m=6
+# n=2
+# print(m%n)
+
+# m=13
+# n=2
+# print(m%n)
+
+# p=15
+# q=7
+# print(p**q)
+
+# p=9
+# q=2
+# print(p**q)
+
+# p=8
+# q=3
+# print(p**q)
