85e1e0933e89d1e77d55122a65dcaa5abdb4fb39
mugdha2611 <mugdha.borhade26@gmail.com>
2021-04-12 15:21:39 +0530

    Ok

---
+++ mugdha-borhade/CLASS/REVERSING_NUMBER.py
@@ -0,0 +1,28 @@
+# REVERSE USING WHILE LOOP:
+
+# num = 1234
+# rev = 0
+# while (num > 0) :
+#     a = num % 10
+#     rev = rev * 10 + a
+#     num = num // 10
+# print(rev)
+
+#  REVERSE BY CONVERTING TO STRING
+
+# n = 5678
+# rev = str(n)
+# print(rev)
+# print(type(rev))
+# a = rev[::-1]
+# print(a)
+# b= int(a)
+# print(b)
+# print(type(b))
+
+#
+# x = 5
+# x |= 3
+# print(x)
+
+# print(4|5)
