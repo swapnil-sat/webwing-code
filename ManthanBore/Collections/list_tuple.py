464fdc2e902a232fa405924f388bc6fbd1a14fa0
manthanbore <mantha.bore@gmail.com>
2021-04-19 13:38:34 +0530

    BRAVO

---
+++ ManthanBore/Collections/list_tuple.py
@@ -0,0 +1,52 @@
+# s="U T"
+# print(s.split())
+# l=[1,2,3,4,5,6]
+# print(l[-1])
+# print(l[::-1])
+# print(l[3])
+# l.append(7)
+# print(l)
+# l.insert(2,30)
+# print(l)
+# l.insert(30,22)
+# print(l)
+# l.insert(-3,33)
+# print(l)
+# l[3]=44
+# print(l)
+# l1=[1,2,3,4]
+# l2=[5,6,7]
+# l1.extend(l2)
+# print(l1)
+# del l[2]
+# print(l)
+# l.pop()
+# print(l)
+# l.pop(-1)
+# print(l)
+# l.clear()
+# print(l)
+# l=[1,2,3,4]
+# l.remove(4)
+# print(l)
+# print(l.count(1))
+# l=[1,2,3,42,3,1,3,4,5,3,4,5]
+# print(l.count(3))
+# print(l.count(100))
+# print(l.index(42))
+# print(len(l))
+# l1=l.sort()
+# print(l)
+# l2=[i for i in l if i%2==0]
+# print(l2)
+
+# t=(1,2,3,4,5)
+# print(t.index(2))
+# print(t.count(2))
+# print(t.count(20))
+# print(t[1])
+# print(t[-1])
+# print(t[0:3])
+# t=(1,2,3,4,5,2,3,2)
+# l=[i for i in t]
+# print(l)
---
+++ ManthanBore/Collections/set_dict.py
@@ -0,0 +1,114 @@
+# s=set()
+# print(type(s))
+# s1={}
+# print(type(s1))
+# s1={1,}
+# print(type(s1))
+# s1=set()
+# print(type(s1))
+# s1.add(20)
+# print(s1)
+# print(type(s1))
+# s1.add(30)
+# print(s1)
+# s2=set()
+# s2.add(40)
+# s2.add(50)
+# print(s2)
+# s1.update(s2)
+# print(s1)
+# l=[1,2,3,4]
+# s1.update(l)
+# print(s1)
+# l=[1,2,3,4,5,2,4]
+# s3=set(l)
+# print(s3)
+# s4=s3
+# print(s4)
+# s4.add(6)
+# print(s4)
+# print(s3)
+# s5=s3.copy()
+# print(s5)
+# s3.add(7)
+# print(s3)
+# print(s5)
+# print(s5.pop())
+# print(s5)
+# s5.remove(6)
+# print(s5)
+# s5.discard(3)
+# print(s5)
+# s3.discard(50)
+# print(s3)
+# s5.discard(50)
+# print(s5)
+# s5.clear()
+# print(s5)
+# print(40 in s1)
+# print(100 not in s1)
+# print(2 not in s1)
+# d1=s1.discard(4)
+# print(s1)
+# d1=s1.remove(3)
+# print(s1)
+# d1=s1.pop()
+# print(d1)
+# i=iter(s1)
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# for i in s1 :
+#     print(i)
+
+# d=dict()
+# print(type(d))
+# d1={}
+# print(type(d1))
+# d={1:"One",2:"Two",3:"Three",4:"Four",5:"Five"}
+# print(d)
+# t=(1,2,3)
+# print(type(t))
+# d[t]="key is a tuple"
+# print(d)
+# d[6]="six"
+# print(d)
+# d[1]="Ten"
+# print(d)
+# d[1]="One"
+# print(d)
+# print(d.items())
+# print(d.keys())
+# print(d.values())
+# for key,value in d.items():
+#     print("key=",key,"value=",value)
+# for a,b in d.items():
+#     print(a,":",b)
+# print(d[1])
+# print(d[2])
+# print(d.get(1))
+# d.get(40)
+# print(d)
+# print(d.get(40,"Rock"))
+# print(d.pop(3))
+# print(d)
+# print(d.popitem())
+# print(d)
+# del d[1]
+# print(d)
+# d["dict"]="Pune"
+# d["state"]="MH"
+# print(d)
+# for k,v in d.items():
+#     if v=="Pune":
+#         d.pop(k)
+#         print(d)
+# d.clear()
+# print(d)
+
+
+
+
+
