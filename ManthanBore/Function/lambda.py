df210cfcfa0ef21bad126e13230bfa803ff04a6e
manthanbore <mantha.bore@gmail.com>
2021-05-19 11:08:58 +0530

    PAUL

---
+++ ManthanBore/Function/lambda.py
@@ -0,0 +1,17 @@
+# x=lambda a:a+9
+# z=x(7)
+# print(z)
+
+# x=lambda a:a+9
+# print(x(7))
+
+# x=lambda a,b:a*b
+# print(x(7,7))
+
+# x=lambda a,b,c:a*b*c
+# print(x(1,2,3))
+
+# add_sub=lambda x,y:(x+y,x-y)
+# a,s=add_sub(3,4)
+# print(a)
+# print(s)
---
+++ ManthanBore/Function/recursion.py
@@ -0,0 +1,29 @@
+# def demo():
+#     print("hello")
+#     demo1()
+# def demo1():
+#     print("webwing")
+# demo()
+# def xyz():
+#     print("working in recursion is easy")
+# xyz()
+# Factorial:-
+# def factorial1(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return n*factorial1(n-1)
+# n=int(input("enter number"))
+# result=factorial1(n)
+# print(result)
+
+# Sum of n number:-
+# def sum(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return n*sum(n-1)
+# n=int(input("enter number"))
+# result=sum(n)
+# print(result)
+
