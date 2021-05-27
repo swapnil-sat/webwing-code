5965642eef31431f644ae01af38e8e720833d8cc
manthanbore <mantha.bore@gmail.com>
2021-05-27 10:58:56 +0530

    CHRIS

---
+++ ManthanBore/Classes Of Python/Inheritance/multi_level_inheritance.py
@@ -0,0 +1,19 @@
+# class Parent(object):
+#     def show_P(self):
+#         print("parent class method")
+# class Child(Parent):
+#     def __init__(self):
+#         print("child class constructor")
+#     def show_C(self):
+#         print("child class method")
+# class Grandchild(Child):
+#     def __init__(self):
+#         print("grandchild class constructor")
+#     def show_G(self):
+#         print("grandchild class method")
+# g=Grandchild()
+# g.show_G()
+# g.show_P()
+# g.show_C()
+
+
---
+++ ManthanBore/Classes Of Python/Inheritance/single_inheritance.py
@@ -0,0 +1,33 @@
+# class Parent(object):
+#     money=1000000
+#     def show(self):
+#         print("parent class instance method")
+#     @classmethod
+#     def show_money(cls):
+#         print("parent class method",cls.money)
+#     @staticmethod
+#     def static():
+#         x=7
+#         print("parent class static method",x)
+# class Child(Parent):
+#     def display(self):
+#         print("child class instance method")
+# c=Child()
+# c.display()
+# c.show()
+# c.show_money()
+# c.static()
+
+# Single Inheritance using constructor:-
+# class Parent(object):
+#     def __init__(self,fname,lname):
+#         self.fname=fname
+#         self.lname=lname
+#     def print_name(self):
+#         print(self.fname,self.lname)
+# class Child(Parent):
+#     pass
+# x=Child("rohit","patil")
+# x.print_name()
+
+
